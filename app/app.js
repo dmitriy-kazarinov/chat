9052463c0df5d5b60f2446ac8b18b7690101e280
 Dmitry <dmitry@Dmitrys-MacBook-Pro.local>
2016-09-28 21:38:01 +0300

    Refactoring components and server

--- app/app.js
+++ app/app.js
@@ -1,220 +1,133 @@
 window.ee = new EventEmitter();
-var my_news = [
-  {
-    author: 'author1',
-    text: 'text1',
-	bigText: 'bigText1'
-  },
-  {
-    author: 'author2',
-    text: 'text2',
-	bigText: 'bigText2'
-  },
-  {
-    author: 'author3',
-    text: 'text3',
-	bigText: 'bigText3'
-  }
-];
+const messages = [];
 
-var Article = React.createClass({
-  getInitialState: function() {
-    return {
-      visible: false,
-      rating: 0
-    };
-  },
-  readmoreClick: function(e) {
-    e.preventDefault();
-    this.setState({visible: true});
-  },
-  addRating: function(){
-  	this.setState({rating: ++this.state.rating}, function(){
-  		console.log(this);
-  	});
-  },
+let Message = React.createClass({
   render: function() {
     var author = this.props.data.author,
-        text = this.props.data.text,
-		bigText = this.props.data.bigText,
-		visible = this.state.visible,
-		rating = this.state.rating;
+        text = this.props.data.text;
 
     return (
-      <div className='article'>
-        <p className='news__author'>{author}:</p>
-        <p className='news__text'>{text}</p>
-		<a href="#"
-			onClick={this.readmoreClick}
-			className={'news__readmore ' + (visible ? 'none': '')}>
-			More
-		</a>
-		<p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
-		<button onClick={this.addRating}>{rating}</button>
+      <div className='message'>
+        <p className='message__author'>{author}:{text}</p>
       </div>
     )
   }
 });
 
-var Add = React.createClass({
+let AddMessage = React.createClass({
 	getInitialState: function(){
 		return {
 			author: '',
 			text: '',
-			accept: true,
 			emptyAuthor: true,
 			emptyText: true
 		}
 	},
 	updateAuthor: function(event){
-		var self = this;
-		var target = event.target;
+		let self = this;
+		let target = event.target;
 		if(target.value.length){
 			self.setState({emptyAuthor: false})
 		} else {
 			self.setState({emptyAuthor: true})
 		}
-		self.setState({author: event.target.value});
+		self.setState({author: target.value});
 	},
 	updateAuthorText: function(event){
-		var self = this;
-		var target = event.target;
+		let self = this;
+		let target = event.target;
 		if(target.value.length){
 			self.setState({emptyText: false})
 		} else {
 			self.setState({emptyText: true})
 		}
-		this.setState({text: event.target.value});
+		this.setState({text: target.value});
 	},
-	showInfoAuthor: function(){
-		var item = [{
+	addData: function(e){
+    e.preventDefault();
+		let item = [{
 		    author: this.state.author,
-		    text: this.state.text,
-		    bigText: '...'
+		    text: this.state.text
 		  }];
 
-  		window.ee.emit('News.add', item);
-  		this.state.text = '';
-	},
-	addAccept: function(){
-		if(!this.state.accept){
-			this.setState({accept: true})
-		} else {
-			this.setState({accept: false})
-		}
-
+      socket.emit('messages', item);
+  		window.ee.emit('Messages.add', item);
+      this.setState({
+        author: '',
+  			text: '',
+  			emptyAuthor: true,
+  			emptyText: true
+      });
 	},
 	render: function(){
 		return (
 			<div>
-				<form className='add cf'>
+				<form className='add-message'>
 					<input type="text" onChange={this.updateAuthor} value={this.state.author} />
 					<textarea onChange={this.updateAuthorText} value={this.state.text}></textarea>
-					<input type="checkbox" onChange={this.addAccept} value={this.state.accept} />
-					<button disabled={this.state.accept || this.state.emptyAuthor || this.state.emptyText} onClick={this.showInfoAuthor}>Add data</button>
+					<button disabled={this.state.emptyAuthor || this.state.emptyText} onClick={this.addData}>Add message</button>
 				</form>
 			</div>
 		)
 	}
 });
 
-
-// var TestInput = React.createClass({
-// 	getInitialState: function(){
-// 		return {
-// 			inputText: "Test text"
-// 		}
-// 	},
-// 	componentDidMount: function() { //add focus to input
-//      	ReactDOM.findDOMNode(this.refs.inputTextUncontrol).focus();
-//     },
-// 	changeInput: function(e){
-// 		this.setState({inputText: e.target.value});
-// 	},
-// 	showInfo: function(){
-// 		alert(this.state.inputText);
-// 	},
-// 	showInfoUncontrol: function(){
-// 		console.log(this.refs);
-// 		alert(ReactDOM.findDOMNode(this.refs.inputTextUncontrol).value);
-// 	},
-// 	render: function(){
-// 		return (
-// 			<div>
-// 				<div className="input-row">
-// 					<input
-// 					type="text"
-// 					className='test-input'
-// 					value={this.state.inputText}
-// 					onChange={this.changeInput} />
-// 					<button onClick={this.showInfo}>Show info</button>
-// 				</div>
-// 				<div className="input-row">
-// 					<input
-// 					type="text"
-// 					className='test-input'
-// 					defaultValue="Default text"
-// 					ref="inputTextUncontrol" />
-// 					<button
-// 						onClick={this.showInfoUncontrol}
-// 						ref="buttonTest">Show info</button>
-// 				</div>
-// 			</div>
-// 		);
-// 	}
-// });
-
-var News = React.createClass({
+let Messages = React.createClass({
   render: function() {
-    var data = this.props.data;
-    var newsTemplate;
+    let data = this.props.data;
+    let messagesTemplate;
 
     if (data.length > 0) {
-      newsTemplate = data.map(function(item, index) {
+      messagesTemplate = data.map((item, index) => {
         return (
           <div key={index} >
-          	<Article data={item} />
+          	<Message data={item} />
           </div>
         )
       })
     } else {
-      newsTemplate = <p>No news</p>
+      messagesTemplate = <p>Sorry, No messages</p>
     }
 
     return (
-      <div className='news'>
-        // <TestInput />
-    	{newsTemplate}
-        <strong className={'news__count ' + (data.length > 0 ? '':'none') }>All news: {data.length}</strong>
+      <div className='messages-template'>
+    	{messagesTemplate}
+        <strong className={'messages__count ' + (data.length > 0 ? '':'none') }>All messages: {data.length}</strong>
       </div>
     );
   }
 });
 
-var App = React.createClass({
+// socket.on('messages', function(data){
+//   let updateMessages = newMessages.push(data);
+//   this.setState({messages: updateMessages});
+// });
+
+let App = React.createClass({
   getInitialState: function() {
     return {
-      news: my_news
+      messages: messages
     };
   },
   componentDidMount: function() {
-    /* update this.state.news*/
-    var self = this;
-	  window.ee.addListener('News.add', function(item) {
-	    var nextNews = item.concat(self.state.news);
-	    self.setState({news: nextNews});
+    /* update this.state.messages*/
+    let self = this;
+	  window.ee.addListener('Messages.add', (item) => {
+	    let newMessages = item.concat(self.state.messages);
+      self.setState({messages: newMessages});
+
 	  });
   },
   componentWillUnmount: function() {
-    /* news done */
-    window.ee.removeListener('News.add');
+    /* messages done */
+    window.ee.removeListener('Messages.add');
   },
   render: function() {
     return (
       <div className='app'>
-      	<Add />
-        <h3>News</h3>
-        <News data={this.state.news} />
+      	<AddMessage />
+        <h3>Messages</h3>
+        <Messages data={this.state.messages} />
       </div>
     );
   }
--- server.js
+++ server.js
@@ -8,19 +8,21 @@ app.use('/app', express.static('app'));
 
 io.on('connection', (client) => {
   console.log(`Client connected...`);
-  client.on('join', (name) => {
-    client.nickName = name;
+
+  client.on('messages', (data = []) => {
+    let dataInfo = data[0];
+    let {author, text} = dataInfo;
+
+    client.broadcast.emit('messages', dataInfo);
+    client.emit('messages', dataInfo);
   });
-  client.on('messages', (message) => {
-    let nickName = client.nickName;
-    console.log(`${nickName}: ${message}`);
-    client.broadcast.emit('messages', `${nickName}: ${message}`);
-    client.emit('messages', `${nickName}: ${message}`);
-  });
+
 });
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/app/index.html`);
 });
 
-server.listen(8888);
+server.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
